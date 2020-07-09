fc5d54240c075a3f4c3f65710622e4fb83fd2cc0
jasondyoung <1145809950@qq.com>
2020-07-09 21:45:41 +0800

    clone提交

--- git-demo/src/main/java/cn/ly/gitdemo/controller/HelloController.java
+++ git-demo/src/main/java/cn/ly/gitdemo/controller/HelloController.java
@@ -11,6 +11,7 @@ public class HelloController {
         System.out.println("创建分支");
         System.out.println("主干提交");
         System.out.println("分支提交");
-        return "hello git";
+        System.out.println("clone提交");
+        return "hello git111";
     }
 }
