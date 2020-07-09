0e09838606d1551365c4c941aa010e5b22a90934
Jason_d_Young <luyang1145809950@gmail.com>
2020-07-09 21:28:00 +0800

    Update HelloController.java

--- git-demo/src/main/java/cn/ly/gitdemo/controller/HelloController.java
+++ git-demo/src/main/java/cn/ly/gitdemo/controller/HelloController.java
@@ -11,6 +11,7 @@ public class HelloController {
         System.out.println("创建分支");
         System.out.println("主干提交");
         System.out.println("分支提交");
+        System.out.println("仓库更新");
         return "hello git";
     }
 }
