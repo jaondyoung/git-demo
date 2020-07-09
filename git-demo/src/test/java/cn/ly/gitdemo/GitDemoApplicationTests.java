76ec98f6456cfd71a52a4577e1605f02e6a27a5f
jasondyoung <1145809950@qq.com>
2020-07-09 20:48:16 +0800

    提交到github

---
+++ git-demo/src/test/java/cn/ly/gitdemo/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ly.gitdemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
